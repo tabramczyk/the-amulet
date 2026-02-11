1d6fa4d8c432638d79077b8e9d959ad95e72f56b
Tomasz Abramczyk <tomasz.abramczyk@ringieraxelspringer.pl>
2026-02-11 21:24:09 +0100

    Actions.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/the-amulet/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
